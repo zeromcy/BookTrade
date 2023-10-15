89342fa54ac91334628a688322c5905a9832fe0b
mcy <12407@qq.com>
2023-10-15 13:29:51 +0800

    初始化

---
+++ Trade/src/test/java/com/zjf/TradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
